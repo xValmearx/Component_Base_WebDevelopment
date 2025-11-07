a7a82f25d57f96ed53b8345c10290b6da7dcc859
caleb taylor <calebs.phone2017@gmail.com>
2025-11-07 17:46:24 -0500

    connected style sheet to HTML page and installed more extentions to make writing code easier

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     
-    template_name = "base.html"
+    template_name = "home.html"
---
+++ static/JavaScript/Main.js
@@ -0,0 +1,1 @@
+// Custome Code Here
