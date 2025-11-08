42187862e2fa767c0f00ab46d58913821e6dc15e
caleb taylor <calebs.phone2017@gmail.com>
2025-11-08 17:19:39 -0500

    created functionality to set up custom html tag and custom css

--- static/JavaScript/Main.js
+++ static/JavaScript/Main.js
@@ -1,1 +1,21 @@
 // Custome Code Here
+
+
+
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+    <nav class="navbar">
+        <div class="nav_logo">Testing Components</div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>`;
+  }
+}
+
+customElements.define('nav-bar', NavBar);
